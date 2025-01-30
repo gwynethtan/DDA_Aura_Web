3d39fbfc3acc686b8845799ce5a9ca5eb5e01487
gwynethtan <S10257317@connect.np.edu.sg>
2025-01-30 18:34:39 +0800

    Regrouped files

---
+++ others/tailwind.config.js
@@ -0,0 +1,25 @@
+// Example `tailwind.config.js` file
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+
+    }
+  },
+  variants: {
+    extend: {
+      
+    }
+  }
+}
