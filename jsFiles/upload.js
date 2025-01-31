970ebcaffc449920060602ccbbf54a36769335ca
gwynethtan <S10257317@connect.np.edu.sg>
2025-01-31 14:18:04 +0800

    Renamed firebase.js import files

--- jsFiles/upload.js
+++ jsFiles/upload.js
@@ -2,7 +2,7 @@ const supabaseUrl = "https://qabrcgzafrzbwrtrezqc.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhYnJjZ3phZnJ6YndydHJlenFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzNTE1MjksImV4cCI6MjA1MTkyNzUyOX0.T7CTt9sVsRIg_zhrUokttmz_FDqeRT4Cocw9vDxqTfM";
 const client = supabase.createClient(supabaseUrl, supabaseAnonKey);
-import { firebaseUpload } from 'jsFiles/firebase.js';
+import { firebaseUpload } from "/DDA_Aura_Web/jsFiles/firebase.js";
 
 document.getElementById("uploadButton").addEventListener("click", uploadFile);
 
@@ -18,20 +18,22 @@ async function uploadFile() {
         alert('Please select a file.'); 
         return; 
     } 
-  
-    // Make a unique file name by prefixing it with the timestamp 
-    const fileName = `${Date.now()}-${file.name}`;
+    else{
+      // Make a unique file name by prefixing it with the timestamp 
+      const fileName = `${Date.now()}-${file.name}`;
 
-    // Upload the file to Supabase 
-    const { data, error } = await client.storage 
-        .from('images')// Replace 'images' with your bucket name 
-        .upload(fileName, file); 
-  
-    if (error) {
-      alert("Upload failed:", error);
-      return;
+      // Upload the file to Supabase 
+      const { data, error } = await client.storage 
+          .from('images')
+          .upload(fileName, file); 
+    
+      if (error) {
+        alert("Upload failed:", error);
+        return;
+      }
+      const publicUrlData = client.storage.from("images").getPublicUrl(fileName);
+      const publicUrl = publicUrlData.data.publicUrl;
+      firebaseUpload(publicUrl);
     }
-    const publicUrlData = client.storage.from("images").getPublicUrl(fileName);
-    const publicUrl = publicUrlData.data.publicUrl;
-    firebaseUpload(publicUrl);
+
   }
