349f2468664080bc877208af9b8760ddadbf1f87
gwynethtan <S10257317@connect.np.edu.sg>
2025-02-06 00:29:28 +0800

    User can update thought in textbox

--- jsFiles/profile.js
+++ jsFiles/profile.js
@@ -1,8 +1,38 @@
-import { rankData } from "/DDA_Aura_Web/jsFiles/firebase.js";
+import { updateData } from "/DDA_Aura_Web/jsFiles/firebase.js";
 
-const sortAuraButton = document.getElementById("sortAura");
-sortAuraButton.addEventListener("click", () =>rankData("aura"));
+const editThoughtButton=document.getElementById("editThought")
+editThoughtButton.addEventListener("click",  () =>editThought());
+let textbox = document.getElementById("accountThought");
 
-// When the capture button is clicked, call capturePhoto() function
-const sortLikesButton = document.getElementById("sortLikes");
-sortLikesButton.addEventListener("click", () =>rankData("thoughtLikes"));
+function editThought() {
+    if(textbox.value=="Write my thoughts"){
+        textbox.value="";
+    }
+    
+    if (textbox.hasAttribute("readonly")) {
+        textbox.removeAttribute("readonly");
+        textbox.focus();
+        editThoughtButton.innerHTML = `<img src="/DDA_Aura_Web/images/tick.png" alt="Save" class="w-8 h-8">`;
+    } else {
+        updateData("thoughtDetails","thought",textbox.value);
+        updateData("thoughtDetails","dateWritten",Math.floor(Date.now() / 1000));
+        if(textbox.value==""){
+            textbox.value="Write my thoughts";
+        }
+        textbox.setAttribute("readonly", true);
+        editThoughtButton.innerHTML = `<img src="/DDA_Aura_Web/images/edit.png" alt="Edit" class="w-8 h-8">`;
+    }
+
+    const userText = textbox.value;
+    console.log("User's input:", userText);
+
+};
+
+function autoResize(textarea) {
+    textarea.style.height = "auto";
+    textarea.style.height = Math.min(textarea.scrollHeight, 150) + "px"; // Max height of 300px
+}
+
+
+window.addEventListener("load", () => autoResize(textbox));
+textbox.addEventListener("input", () => autoResize(textbox));
